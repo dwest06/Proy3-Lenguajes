cd723b7f786cbad9336d3b0300727c6912200cad
Christian Oliveros <christianol_01@hotmail.com>
2018-12-22 18:48:30 -0400

    Jsut about to play

--- UI.rb
+++ UI.rb
@@ -2,9 +2,6 @@
 require './Partidas.rb'
 
 Shoes.app(title: "Piedra, Papel, Tijeras, Lagarto, Spock") {
-
-    background("#EFC")
-    border("#BE8", strokewidth: 6)
     
     # General Config
     @players_background = "#FF9499"
@@ -15,9 +12,13 @@ Shoes.app(title: "Piedra, Papel, Tijeras, Lagarto, Spock") {
     @players_jugadas = {
         "Piedra" => :Piedra, "Papel" => :Papel, 
         "Tijera" => :Tijera, "Lagarto" => :Lagarto, "Spock" => :Spock}
+    @players_modos = {
+        "Rondas" => :Rondas, "Alcanzar Puntaje" => :Alcanzar
+    }
 
     # P1 GUI
     @p1_player_name = :P1
+    @p1_score = nil
     @p1_estrategia_selector = nil
     @p1_estrategia_text = nil
     @p1_estrategia = nil # Symbol
@@ -28,6 +29,7 @@ Shoes.app(title: "Piedra, Papel, Tijeras, Lagarto, Spock") {
 
     # P2 GUI
     @p2_player_name = :P2
+    @p2_score = nil
     @p2_estrategia_selector = nil
     @p2_estrategia_text = nil
     @p2_estrategia = nil # Symbol
@@ -53,7 +55,41 @@ Shoes.app(title: "Piedra, Papel, Tijeras, Lagarto, Spock") {
     @iniciar_juego = nil
     @reiniciar_juego = nil
     @detener_juego = nil
+    @rondas_juego = nil
+    @modo_juego_selector = nil
+    @modo_juego = nil
+    @cantidad_editline_juego = nil
+    @cantidad_juego = 1
+    @iniciar_partida_juego = nil
+
+    # Coloca un Puntaje a un jugador
+    def set_score(contenedor, player_name, puntos)
+        contenedor.text = "Puntos de #{@manual_player_string_name[player_name]}: #{puntos}"
+    end
+
+    # Coloca el numero de la Ronda Actual
+    def set_round(ronda)
+        @rondas_juego.text = "Ronda: #{ronda}"
+    end
+
+    # Obtener la jugada previa de un jugador
+    def get_prev_play(player_name)
+        @juego_partida.prev_play[player_name]
+    end
     
+    # Actualiza el ui de los puntajes, la ronda actual y las imagenes de las manos (TODO)
+    def update_game_ui()
+        set_score(@p1_score, @p1_player_name, @juego_partida.resultados[@p1_player_name])
+        set_score(@p2_score, @p2_player_name, @juego_partida.resultados[@p2_player_name])
+        set_round(@juego_partida.resultados[@juego_partida.round_name])
+    end
+
+    def state_rounds_options(state)
+        @modo_juego_selector.state = state
+        @cantidad_editline_juego.state = state
+        @iniciar_partida_juego.state = state
+    end
+
     # Revisa un string y solo deja los caracteres validos de un float sin signo
     def check_float(t) 
         t.gsub(/[^\d\.]+/, '').squeeze(".")
@@ -68,12 +104,17 @@ Shoes.app(title: "Piedra, Papel, Tijeras, Lagarto, Spock") {
         end
         return t
     end
+
+    # Revisa un string y solo deja los caracteres validos de un integer sin signo
+    def check_int(t) 
+        t.gsub(/[^\d]+/, '')
+    end
     
     # Genera las opciones de la estrategia uniforme
     def generate_uniform_options()
         checks = @players_jugadas.keys.map do |name|
-            flow { c = check; para name }
-            [c, name]
+            flow { @c = check; para name }
+            [@c, name]
         end
         para "Se tiene que seleccionar por lo menos una opcion", size: "xx-small"
         return checks
@@ -104,6 +145,7 @@ Shoes.app(title: "Piedra, Papel, Tijeras, Lagarto, Spock") {
     # Genera las opciones de la estrategia sesgada
     def generate_bias_options()
         checks = @players_jugadas.keys.map do |name|
+            c = n = nil
             flow { c = check; para name; n = edit_line "1.0", width: 100, margin: 4, align: "center";}
             n.change { |t|
                 t.text = check_float(t.text()) 
@@ -244,84 +286,120 @@ Shoes.app(title: "Piedra, Papel, Tijeras, Lagarto, Spock") {
         }
     end
 
+    background("#EFC")
     # Control Central
-    stack(margin: 6, width: '100%', height: '100%') do
-        flow do
-            # Izquierda
-            stack :width => '50%', :margin => 6 do
-                background @players_background
-                border(@players_border, strokewidth: 6)
-                stack(margin: 6) do
-                    title "Jugador 1"
-                    subtitle("Seleccionar Estrategia", size: "small", weight: "bold")
-                    @p1_estrategia_selector = list_box items: @players_estrategias.keys,
-                        width: 120, choose: @players_estrategias.keys[0], margin_left: 4 do |list|
-                        @p1_estrategia_text.text = "Estrategia #{list.text} Selecionada"
+    stack(width: '100%', height: '100%', scroll: true) do
+        stack(margin_right: gutter()) do
+            background("#EFC")
+            border("#BE8", strokewidth: 6)
+            flow(margin: 6, margin_bottom: 0) do
+                # Izquierda
+                stack :width => '50%', :margin => 6 do
+                    background @players_background
+                    border(@players_border, strokewidth: 6)
+                    stack(margin: 6) do
+                        title "Jugador 1"
+                        subtitle("Seleccionar Estrategia", size: "small", weight: "bold")
+                        @p1_estrategia_selector = list_box items: @players_estrategias.keys,
+                            width: 120, choose: @players_estrategias.keys[0], margin_left: 4 do |list|
+                            @p1_estrategia_text.text = "Estrategia #{list.text} Selecionada"
 
-                        @p1_estrategia = @players_estrategias[list.text]
+                            @p1_estrategia = @players_estrategias[list.text]
 
-                        @p1_strategy_stack.hidden = @p1_estrategia == :Pensar
-                        @p1_strategy_stack.clear()
-                        if !@p1_strategy_stack.hidden
-                            @p1_strategy_stack.append do
-                                subtitle("Configurar Estrategia", size: "small", weight: "bold") 
-                                case @p1_estrategia
-                                    when :Uniforme
-                                        @p1_uniform_options = generate_uniform_options()
-                                    when :Sesgada
-                                        @p1_bias_options = generate_bias_options()
-                                    when :Copiar
-                                        @p1_copy_options = generate_copy_options()
-                                    when :Manual
-                                        generate_manual_options(@p1_player_name)
-                                end 
+                            @p1_strategy_stack.hidden = @p1_estrategia == :Pensar
+                            @p1_strategy_stack.clear()
+                            if !@p1_strategy_stack.hidden
+                                @p1_strategy_stack.append do
+                                    subtitle("Configurar Estrategia", size: "small", weight: "bold") 
+                                    case @p1_estrategia
+                                        when :Uniforme
+                                            @p1_uniform_options = generate_uniform_options()
+                                        when :Sesgada
+                                            @p1_bias_options = generate_bias_options()
+                                        when :Copiar
+                                            @p1_copy_options = generate_copy_options()
+                                        when :Manual
+                                            generate_manual_options(@p1_player_name)
+                                    end 
+                                end
                             end
                         end
+                        @p1_estrategia_text = para "Estrategia no seleccionada"
+                        @p1_strategy_stack = stack(hidden: true) { }
                     end
-                    @p1_estrategia_text = para "Estrategia no seleccionada"
-                    @p1_strategy_stack = stack(hidden: true) { }
                 end
-            end
-            # Derecha
-            stack :width => '50%', :align => "right", :margin => 6 do
-                background @players_background
-                border(@players_border, strokewidth: 6)
-                stack(margin: 6) do
-                    title "Jugador 2"
-                    subtitle("Seleccionar Estrategia", size: "small", weight: "bold")
-                    @p2_estrategia_selector = list_box items: @players_estrategias.keys,
-                        width: 120, choose: @players_estrategias.keys[0], margin_left: 4 do |list|
-                        @p2_estrategia_text.text = "Estrategia #{list.text} Selecionada"
+                # Derecha
+                stack :width => '50%', :align => "right", :margin => 6 do
+                    background @players_background
+                    border(@players_border, strokewidth: 6)
+                    stack(margin: 6) do
+                        title "Jugador 2"
+                        subtitle("Seleccionar Estrategia", size: "small", weight: "bold")
+                        @p2_estrategia_selector = list_box items: @players_estrategias.keys,
+                            width: 120, choose: @players_estrategias.keys[0], margin_left: 4 do |list|
+                            @p2_estrategia_text.text = "Estrategia #{list.text} Selecionada"
 
-                        @p2_estrategia = @players_estrategias[list.text]
+                            @p2_estrategia = @players_estrategias[list.text]
 
-                        @p2_strategy_stack.hidden = @p2_estrategia == :Pensar
-                        @p2_strategy_stack.clear()
-                        if !@p2_strategy_stack.hidden
-                            @p2_strategy_stack.append do
-                                subtitle("Configurar Estrategia", size: "small", weight: "bold") 
-                                case @p2_estrategia
-                                    when :Uniforme
-                                        @p2_uniform_options = generate_uniform_options()
-                                    when :Sesgada
-                                        @p2_bias_options = generate_bias_options()
-                                    when :Copiar
-                                        @p2_copy_options = generate_copy_options()
-                                    when :Manual
-                                        generate_manual_options(@p2_player_name)
-                                end 
+                            @p2_strategy_stack.hidden = @p2_estrategia == :Pensar
+                            @p2_strategy_stack.clear()
+                            if !@p2_strategy_stack.hidden
+                                @p2_strategy_stack.append do
+                                    subtitle("Configurar Estrategia", size: "small", weight: "bold") 
+                                    case @p2_estrategia
+                                        when :Uniforme
+                                            @p2_uniform_options = generate_uniform_options()
+                                        when :Sesgada
+                                            @p2_bias_options = generate_bias_options()
+                                        when :Copiar
+                                            @p2_copy_options = generate_copy_options()
+                                        when :Manual
+                                            generate_manual_options(@p2_player_name)
+                                    end 
+                                end
                             end
                         end
+                        @p2_estrategia_text = para "Estrategia no seleccionada"
+                        @p2_strategy_stack = stack(hidden: true) { }
+                        
                     end
-                    @p2_estrategia_text = para "Estrategia no seleccionada"
-                    @p2_strategy_stack = stack(hidden: true) { }
                 end
             end
-        end
-        flow :margin => 6, :width => '100%' do
-            @iniciar_juego = button "Iniciar Partida", width: '33%'
-            @detener_juego = button "Detener Partida", width: '33%'
-            @reiniciar_juego = button "Reiniciar Partida", width: '33%'
+            flow :margin => 6, :width => '100%', :margin_left => 12, :margin_bottom => 0 do
+                @iniciar_juego = button "Iniciar Partida", width: '33%'
+                @detener_juego = button "Detener Partida", width: '33%'
+                @reiniciar_juego = button "Reiniciar Partida", width: '33%'
+            end
+            flow :margin => 6, :width => '100%', :margin_left => 12, :margin_bottom => 0 do
+                @rondas_juego = title "Ronda: 0", size: 'small', weight: "bold", margin_right: 12, margin_left: 0
+                @p1_score = title("Puntos de Jugador 1: 0", size: "small", 
+                    weight: "bold", margin_right: 12)
+                @p2_score = title("Puntos de Jugador 2: 0", size: "small", 
+                    weight: "bold", margin_right: 6)
+            end
+            
+            flow :margin => 6, :width => '100%', :margin_left => 12, :margin_bottom => 0 do
+                para "Modo de Juego: ", margin_left: 0
+                @modo_juego_selector = list_box items: @players_modos.keys,
+                    width: 120, choose: @players_modos.keys[0], margin_left: 4, state: "disabled" do |list|
+                    @modo_juego = @players_modos[list.text]
+                end
+                @cantidad_editline_juego = edit_line "1", state: "disabled", width: 40, margin_left: 8
+                @cantidad_editline_juego.change { |t|
+                    t.text = check_int(t.text()) 
+                }
+                @iniciar_partida_juego = button "Iniciar Ronda", state: "disabled", margin_left: 4
+                @iniciar_partida_juego.click {
+                    num = @cantidad_editline_juego.text().to_i().abs()
+                    if num > 0
+                        @cantidad_juego = num
+                        @juego_iniciado = true
+                        state_rounds_options("disabled")
+                    else
+                        alert "Para Iniciar las Rondas se requiere un numero mayor a 0"
+                    end
+                }
+            end
         end
     end
 
@@ -337,6 +415,8 @@ Shoes.app(title: "Piedra, Papel, Tijeras, Lagarto, Spock") {
         @detener_juego.state = "disabled"
         @p1_estrategia_selector.state = nil 
         @p2_estrategia_selector.state = nil 
+
+        state_rounds_options("disabled")
 
         case @p1_estrategia
             when :Uniforme
@@ -361,6 +441,10 @@ Shoes.app(title: "Piedra, Papel, Tijeras, Lagarto, Spock") {
                 state_manual_options(@p2_player_name, "disabled")  
                 reset_manual_option(@p2_player_name) 
         end
+
+        set_score(@p1_score, @p1_player_name, 0)
+        set_score(@p2_score, @p2_player_name, 0)
+        set_round(0)
     end
 
     # Iniciar el juego
@@ -428,12 +512,18 @@ Shoes.app(title: "Piedra, Papel, Tijeras, Lagarto, Spock") {
         end
 
         @juego_partida = Partidas::Partida.new(partida_args)
-        @juego_iniciado = true
         @iniciar_juego.state = "disabled" 
         @reiniciar_juego.state = nil
         @detener_juego.state = nil
         @p1_estrategia_selector.state = "disabled"  
         @p2_estrategia_selector.state = "disabled"  
+        state_rounds_options(nil)
+    end
+
+    # Reiniciar Juego
+    def reiniciar_juego()
+        @juego_partida.reiniciar()
+        update_game_ui()
     end
 
     @iniciar_juego.click {
@@ -443,5 +533,15 @@ Shoes.app(title: "Piedra, Papel, Tijeras, Lagarto, Spock") {
     @detener_juego.click {
         terminar_juego()
     }
-   
+
+    @reiniciar_juego.click {
+        reiniciar_juego()
+    }
+
+    last = now = Time.now
+    animate = animate FPS do
+      now = Time.now
+      game.update(now - last)
+      # rendering goes here
+    end
 }
